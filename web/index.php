0d782b7e036c884ed8c42a585a311a9fa910ed89
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-05-03 15:50:18 -0400

    Create Main.css; Set debug preference
    Main.css »
        Create file and add basic styles
    
    index.php »
        Add preference that debugging should be enabled
        Add container element

--- web/index.php
+++ web/index.php
@@ -1,9 +1,17 @@
+<?php
+  // Enable bebugging
+  $app["debug"] = true;
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <title>Confuso</title>
+    <!-- Links -->
+    <link rel="stylesheet" href="../Assets/Stylesheets/Main.css">
   </head>
   <body>
-    <h4>Would this be where I start my site?</h4>
+    <div class="container">
+      <h4>Heroku-PHP</h4>
+    </div> <!-- .container -->
   </body>
 </html>
