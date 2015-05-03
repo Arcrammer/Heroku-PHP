be7ec7569d4c735228171e71588668ab761d2b84
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-05-03 15:43:32 -0400

    Fine. I’ll remove all of it. #Screaming internally

--- web/index.php
+++ web/index.php
@@ -1,19 +1,3 @@
-<?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-$app->run();
-
-?>
 <!DOCTYPE html>
 <html>
   <head>
