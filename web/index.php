aeb882326be2cd44156e2cd6a2c39a29736d0237
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-05-03 15:35:14 -0400

    Still Confuse
    That wasn’t where I put my site.
    
    Maybe it was.
    
    It was still returning ‘Hello’ so I’ve removed all of that PHP. But why
    would I need to remove that!? It wouldn’t be there if I didn’t need it,
    but it was overwriting all of my stuff? I’m so lost.

--- web/index.php
+++ web/index.php
@@ -1,31 +1,9 @@
-<?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
-});
-
-$app->run();
-
-?>
 <!DOCTYPE html>
 <html>
   <head>
     <title>Confuso</title>
   </head>
   <body>
-    <h4>Would this be where I start my site?</h4>
+    <h4><?= "Would this be where I start my site?" ?></h4>
   </body>
 </html>
