ccb2037ddc413efe924dedc8ee69e7b5446c1823
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-05-05 14:06:11 -0400

    Add Metadata Elements
    .htaccess »
        Add RewriteRule to basically convert directory names to file names
    
    Main.css »
        Copy the same styles as those for Heroku-Rails
    
    BasicForm.php »
        Create outline for form

--- web/BasicForm.php
+++ web/BasicForm.php
@@ -10,14 +10,11 @@
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
       <meta id="viewport" name="viewport" content ="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <!-- Links -->
-      <link rel="stylesheet" href="Assets/Stylesheets/Main.css">
+      <link rel="stylesheet" href="../Assets/Stylesheets/Main.css">
   </head>
   <body>
     <div class="container">
-      <h4>Heroku-PHP</h4>
-      <ul>
-        <li><a href="BasicForm/">Basic Form</a></li>
-      </ul>
+      <h4>Basic Form</h4>
     </div> <!-- .container -->
   </body>
 </html>
