5c07ceb9e8d477fd34256514f484c6f4a31bf2bb
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-05-07 12:35:41 -0400

    :sparkles: Complete Basic Form
    I’ve actually added the functionality for it!

--- web/BasicForm/index.php
+++ web/BasicForm/index.php
@@ -15,6 +15,15 @@
   <body>
     <div class="container">
       <h4>Basic Form</h4>
+      <?php if ($_POST): ?>
+      <p>Hello, <?= $_POST['firstName'] ?>!</p>
+      <?php else: ?>
+        <form method="post">
+          <label for="firstName">First name:</label><br />
+          <input type="text" id="firstName" name="firstName"><br />
+          <input type="submit" value="Greet me!">
+        </form>
+      <?php endif; ?>
       <a href="/" class="gmooh">Get me out of here</a>
     </div> <!-- .container -->
   </body>
